7ecdd96e5f5fb008bfcef80aef1b480024767f99
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-11-19 14:08:11 +0000

    Added Shield
    Still slightly buggy shield movement.

---
+++ GameJam 2018 Entry/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 offset;
+    public Transform playerTransform;
+
+	// Initialise camera position
+	void Start () {
+
+        gameObject.transform.position = playerTransform.position + offset;
+
+	}
+	
+	// Update camera to follow player
+	void FixedUpdate () {
+
+        gameObject.transform.position = playerTransform.position + offset;
+
+    }
+}
--- GameJam 2018 Entry/Assets/Scripts/PlayerController.cs
+++ GameJam 2018 Entry/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
+using System;
 
 // Controls all player functions
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public GameObject shield;
+    public float shieldOffset;
+    public float rotSpeed;
+    private float angleLastFrame;
 
-	// Player Movement
-	void FixedUpdate () {
+    private void Start()
+    {
+        angleLastFrame = 0;
+    }
 
+    // Player and Shield Movement
+    void FixedUpdate () {
+
+        // Player WASD movement 
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         gameObject.transform.position += new Vector3( deltaX, deltaY );
+
+        // Shield Movement with mouse  
+
+        // Reset shield rotation 
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 shieldDirection = gameObject.transform.position - mousePos;
+
+        // Shield rotation 
+        float shieldAngle = (float) Math.Acos( shieldDirection.normalized.x );
+        if (shieldDirection.y < 0)
+            shieldAngle = 2 * (float)Math.PI - shieldAngle;
+ 
+        shield.transform.RotateAround(gameObject.transform.position, new Vector3(0, 0, 1), (shieldAngle - angleLastFrame) * rotSpeed );
+        angleLastFrame = shieldAngle;
 
 	}
 }
