daf0f9d5a6eb71cb126cca7e693de4d182a31c2a
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-11-19 15:13:43 +0000

    Added player animation

---
+++ GameJam 2018 Entry/Assets/Scripts/Player Animation/LeftState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftState : StateMachineBehaviour {
+
+	//OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        if (!Input.GetKey("a"))
+            animator.SetBool("Left", false);
+
+	}
+}
