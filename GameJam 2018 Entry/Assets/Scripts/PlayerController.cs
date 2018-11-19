9e9c58391c31f6172624a84facd14b694c67a4d6
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-11-19 13:20:45 +0000

    Added Player Movement
    Moves with WASD

---
+++ GameJam 2018 Entry/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+// Controls all player functions
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Player Movement
+	void FixedUpdate () {
+
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        gameObject.transform.position += new Vector3( deltaX, deltaY );
+
+	}
+}
