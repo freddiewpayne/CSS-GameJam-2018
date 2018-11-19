4cec595b691db0206f238e87d2f3c4802d1a82b0
robertpop99 <robertpop99@gmail.com>
2018-11-19 21:24:37 +0000

    all direction shooting

---
+++ GameJam 2018 Entry/Assets/Scripts/Enemies/All_direction_shoot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class All_direction_shoot : MonoBehaviour {
+
+    Transform player;
+    float timer = 0;
+    public float shoot_timer;
+    public Rigidbody2D projectile;
+    public int n_projectile = 10;
+    public float bullets_speed = 2;
+
+    // Update is called once per frame
+    void Update () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timer += Time.deltaTime;
+        Vector3 playerRotation = player.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, playerRotation);
+
+        if (timer > shoot_timer)
+        {
+            timer = 0;
+            shoot_all_directions();
+
+
+        }
+
+    }
+
+    void shoot_all_directions()
+    {
+        Rigidbody2D clone;
+        float angle = 360 / n_projectile;
+
+        for(int i = 0; i < n_projectile; i++)
+        {
+            Transform direction = transform;
+            direction.rotation = Quaternion.AngleAxis(angle * i, Vector3.forward);
+
+            clone = Instantiate(projectile, direction.position, direction.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.up * bullets_speed);
+        }
+
+
+    }
+}
