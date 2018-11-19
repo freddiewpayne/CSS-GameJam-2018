9aed62b7109d946322451bc151b6ce854c1f6c31
robertpop99 <robertpop99@gmail.com>
2018-11-19 18:51:26 +0000

    more stuff

---
+++ GameJam 2018 Entry/Assets/Scripts/Enemies/Sword_enemy_move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword_enemy_move : MonoBehaviour {
+
+    Transform player;
+    public Rigidbody2D self;
+    public sword_timer = 1;
+    float timer = 0;
+
+	// Update is called once per frame
+	void Update () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        float distance = Mathf.Sqrt(Mathf.Pow(player.position.x - transform.position.x, 2)
+                                           + Mathf.Pow(player.position.y - transform.position.y, 2));
+
+        if (Mathf.Abs(distance) < 2)
+        {
+            if(timer > sword_timer)
+            {
+
+            }
+        }
+
+    }
+}
