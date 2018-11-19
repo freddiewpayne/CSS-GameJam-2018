9bc2cd97534802d88b6650c327c962017494183d
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-11-19 17:17:33 +0000

    Added Maze-Building
    Nearly ready to build maze now

---
+++ GameJam 2018 Entry/Assets/Scripts/Inventory/SlotController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SlotController : MonoBehaviour {
+
+    public int slotNumber;
+    public Image image;
+    private Slot slot;
+
+    // Update the slot sprite
+    private void Update()
+    {
+        try
+        {
+            slot = InventoryController.Instance.slots[slotNumber];
+            image.sprite = slot.sprite;
+            image.gameObject.SetActive(true);
+        }
+        catch (System.Exception) { image.gameObject.SetActive(false); };
+    }
+}
---
+++ GameJam 2018 Entry/Assets/Scripts/MazeMaker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMaker : MonoBehaviour {
+
+    public GameObject wall;
+    public int size;
+    private char[][] maze;
+
+    // Maze is created on Awake 
+    private void Awake()
+    {
+        // Need to set value of 'maze' here
+
+        // Need to turn string of hashes and spaces into a maze 
+        for ( int i = 0; i < size; i ++ )
+        {
+            for (int j = 0; j < size; j++)
+            {
+                // Make a wall if we encounter a '#'
+                if (maze[i][j] == '#')
+                {
+                    Vector3 wallPos = new Vector3( (float) ( i + 0.5 ), (float) ( j + 0.5 ) );
+                    Instantiate(wall, wallPos, wall.transform.rotation);
+                }
+            }
+        }
+
+    }
+
+}
