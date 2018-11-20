d5ac04327ebdafee0060b15d03a5152bc03362c1
dANW34V3R <31134699+dANW34V3R@users.noreply.github.com>
2018-11-20 09:55:04 +0000

    actual random sword and light placement

--- GameJam 2018 Entry/Assets/Scripts/pickupSword.cs
+++ GameJam 2018 Entry/Assets/Scripts/pickupSword.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections.Generic;
-public class PickupController : MonoBehaviour {
+public class pickupSword : MonoBehaviour
+{
 
     public Sprite UISprite;
     public string objectName;
@@ -11,19 +12,19 @@ public class PickupController : MonoBehaviour {
     {
         System.Random ran = new System.Random();
         double randomNo = (ran.Next(0, 1000) / (double)1000);
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 50; i++)
         {
             randomNo = (ran.Next(0, 1000) / (double)1000);
         }
         int pX = (2 * ((Int16)(randomNo * 9) + 1)) + 1;
-        transform.position = new Vector3((float)(pX + 0.5 ), (float)(pX + 0.35 ), 0);
+        transform.position = new Vector3((float)(pX + 0.5), (float)(pX + 0.35), 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            InventoryController.Instance.add( UISprite, objectName );
+            InventoryController.Instance.add(UISprite, objectName);
             Destroy(gameObject);
         }
     }
