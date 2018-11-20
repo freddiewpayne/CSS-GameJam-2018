492eb40a1151bcf5832897a49e9e616da3e9022d
dANW34V3R <31134699+dANW34V3R@users.noreply.github.com>
2018-11-20 08:53:18 +0000

    pickup random placement

--- GameJam 2018 Entry/Assets/Scripts/PickupController.cs
+++ GameJam 2018 Entry/Assets/Scripts/PickupController.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
-
+using System;
+using System.Collections.Generic;
 public class PickupController : MonoBehaviour {
 
     public Sprite UISprite;
     public string objectName;
 
+
+    private void Start()
+    {
+        System.Random ran = new System.Random();
+        float pX = (float)(2 * ((ran.Next(0, 100) / (double)100)  * (MazeGenerator.mazeSize - 1) / 2) + 1);
+        transform.position = new Vector3((float)(pX ), (float)(pX + 1), 0);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
